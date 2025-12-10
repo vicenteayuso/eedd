8b7f2e5da475f4a800b94b0028515804d1ebc78a
Vicente <vicenteayusopi@gmail.con>
2025-12-10 19:11:38 +0100

    App paqueteria

---
+++ aplicacion1/Programa.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Programa {
+
+    // Scanner global
+    static Scanner sc = new Scanner(System.in);
+    static Envio[] envios = new Envio[50];
+    static Integer iEnvios = 0;
+
+    public static void main(String[] args) {
+
+        boolean repetirMenu = true;
+
+        do {
+            String opcion = verMenu();   // verMenu ahora devuelve la opción
+
+            switch (opcion) {
+                case "1":
+                    registrarEnvio();
+                    break;
+
+                case "2":
+                    System.out.println("Mostrando informe...\n");
+                    
+                    break;
+
+                case "X":
+                case "x":
+                    repetirMenu = false;
+                    break;
+
+                default:
+                    System.out.println("Opción desconocida\n");
+            }
+
+        } while (repetirMenu);
+
+        // Esto se ejecuta solo cuando se sale del menú
+        sc.close();
+        System.out.println("Fin del programa. Espero volver a verle pronto.");
+    }
+
+    // verMenu debe devolver la opción
+    public static String verMenu() {
+        System.out.println("APLICACION DE PAQUETERÍA");
+        System.out.println("1. Registrar envío");
+        System.out.println("2. Ver informe de envíos");
+        System.out.println("x. Salir");
+        System.out.print("Opción: ");
+        return sc.nextLine();  // Devuelve lo que el usuario escribió
+    }
+
+    public static void registrarEnvio() {
+        try {
+            System.out.println("\nRegistro de Envío");
+            System.out.print("\tNúmero: ");
+            String numero = sc.nextLine();
+
+            System.out.print("\tPrecio: ");
+            float precio = Float.parseFloat(sc.nextLine());
+
+            Envio envio = new Envio();
+            envio.numero = numero;
+            envio.precio = precio;
+            envios[iEnvios++] = envio;
+
+            System.out.println("Registro OK\n");
+        } catch (Exception excepcion) {
+            System.out.println("Registro KO\n");
+        }
+    }
+}
